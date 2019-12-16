1d1da9876ed4bb2ec6c77b1083bc91cefc18fdec
mthdht <mthdht@gmail.com>
2019-12-16 09:55:52 +0100

    made route for book resource

---
+++ src/router.php
@@ -0,0 +1,35 @@
+<?php
+
+$url = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+if ($url == '/') {
+    require CONTROLLERS . 'HomeController.php';
+    home();
+} elseif ($url == '/livres' && $method == 'GET') {
+    require CONTROLLERS . 'BookController.php';
+    bookIndex();
+}
+elseif ($url == "/livres" && $method == 'POST') {
+    require CONTROLLERS . 'BookController.php';
+    bookStore();
+}
+elseif (preg_match("#^\/livres/nouveau$#", $url) && $method == 'GET') {
+    require CONTROLLERS . 'BookController.php';
+    bookCreate();
+}
+elseif (preg_match("#^\/livres/([A-Za-z0-9-_' ]+)$#", $url, $matches) && $method == 'GET') {
+    require CONTROLLERS . 'BookController.php';
+    bookShow($matches[1]);
+}
+elseif (preg_match("#^\/livres/([A-Za-z0-9-_' ]+)$#", $url, $matches) && $method == 'POST') {
+    require CONTROLLERS . 'BookController.php';
+    bookUpdate($matches[1]);
+}
+elseif (preg_match("#^\/livres/([A-Za-z0-9-_' ]+)\/edit$#", $url, $matches) && $method == 'GET') {
+    require CONTROLLERS . 'BookController.php';
+    bookEdit($matches[1]);
+}
+elseif (preg_match("#^\/livres/([A-Za-z0-9-_' ]+)\/delete$#", $url, $matches) && $method == 'POST') {
+    require CONTROLLERS . 'BookController.php';
+    bookIndex($matches[1]);
+}
