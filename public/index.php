d0d94c6902430352ed4301456a21c4bf251c70ca
mthdht <mthdht@gmail.com>
2019-12-16 09:24:08 +0100

    made index

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+// all config stuff
+require '../src/config/config.php';
+
+// all helper function
+require SRC . 'helper.php';
+
+//database connexion
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=' . DATABASE . ';charset=utf8;' , USER, PASSWORD);
+
+// parsing URL
+require SRC . 'router.php';
+
+run();
