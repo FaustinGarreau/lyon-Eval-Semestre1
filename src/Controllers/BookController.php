0e9863786ed406b2386a6e74f1d95b6701232c04
Fenidorn <tbeniere@edenschool.fr>
2019-12-17 17:25:30 +0100

    ajout de commentaire

--- src/Controllers/BookController.php
+++ src/Controllers/BookController.php
@@ -1,35 +1,43 @@
 <?php
 
 function bookCreate() {
+    // on affiche la page de creation d'un book
     require VIEWS . 'books/create.php';
 }
 
 function bookStore() {
+    // on verifie si sa existe 
     if (isset($_POST["title"]) && isset($_POST['author']) && isset($_POST["slug"]) && isset($_POST["description"]) && isset($_POST["date"]) ) {
-
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["title"])) {
             $_SESSION['errors']["titleErr"] = "Title is required";
         } 
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["author"])) {
             $_SESSION['errors']["authorErr"] = "Author is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["slug"])) {
             $_SESSION['errors']["slugErr"] = "Slug is required";
-        }  
+        } 
+        // on verifie si c'est vide si oui alors on affiche une erreur 
         if (empty($_POST["description"])) {
             $_SESSION['errors']["descriptionErr"] = "Description is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["date"])) {
             $_SESSION['errors']["dateErr"] = "Date is required";
         }
 
-
+        // on verifie si y'a pas d'erreurs si oui on cree le book et on redirige vers la pages des books
         if (!isset($_SESSION["errors"])) {
             require MODELS . "BookModel.php";
             storeBook();
             header("Location: /books");
             exit();
-        } else {
+        } 
+        // sinon on redirige vers la page de creation d'un book et on affiche les erreurs
+        else {
             header('Location: /books/create');
             exit();
         }
@@ -47,6 +55,7 @@ function booksGet() {
 function bookShow($slug) {
     require MODELS . 'BookModel.php';
     $book = getBook($slug);
+        // on verifie si book existe
     if ($book) {
         require VIEWS . 'books/show.php';
     } else {
@@ -56,31 +65,38 @@ function bookShow($slug) {
 
 
 function bookUpdate($slug) {
+    // on verifie si sa existe 
     if (isset($_POST["title"]) && isset($_POST['author']) && isset($_POST["slug"]) && isset($_POST["description"]) && isset($_POST["date"]) ) {
-        
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["title"])) {
             $_SESSION['errors']["titleErr"] = "Title is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["author"])) {
             $_SESSION['errors']["authorErr"] = "Author is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["slug"])) {
             $_SESSION['errors']["slugErr"] = "Slug is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["description"])) {
             $_SESSION['errors']["descriptionErr"] = "Description is required";
         }
+        // on verifie si c'est vide si oui alors on affiche une erreur
         if (empty($_POST["date"])) {
             $_SESSION['errors']["dateErr"] = "Date is required";
         }
 
-
+        // on verifie si y'a pas d'erreurs si oui on met a jour le book et on redirige vers la pages des books
         if (!isset($_SESSION["errors"])) {
             require MODELS . "BookModel.php";
             updateBook($slug);
             header("Location: /books");
             exit();
-        } else {
+        } 
+        // sinon on redirige vers la page de modification avec les erreurs
+        else {
             header('Location: /books/' . $slug . '/edit');
             exit();
         }
@@ -92,6 +108,7 @@ function bookUpdate($slug) {
 function bookEdit($slug) {
     require MODELS . 'BookModel.php';
     $book = getBook($slug);
+    // on verifie si book existe 
     if ($book) {
         require VIEWS . 'books/edit.php';
     } else {
@@ -101,6 +118,7 @@ function bookEdit($slug) {
 
 
 function bookDelete($slug) {
+    // on va cherche dans le model la function deleteBook($slug) pour supprimer ce book puis on redirige vers la page des books
     require MODELS . 'BookModel.php';
     deleteBook($slug);
     header('Location: /books');
--- src/router.php
+++ src/router.php
@@ -7,37 +7,37 @@ function run() {
     if ($url == '/') {
 
     } 
-
+// on verifie si l'url = /books/nomDuBook/delete et que la requete est en methode post
     elseif (preg_match('#^\/books\/([a-zA-Z0-9_-]+)\/delete$#',$url,$match) && $method == "POST") {
         require CONTROLLERS . 'BookController.php';
         bookDelete($match[1]);
     } 
-
+// on verifie si l'url = /books/nomDuBook et que la requete est en methode post
     elseif (preg_match('#^\/books\/([a-zA-Z0-9_-]+)$#',$url,$match) && $method == "POST") {
         require CONTROLLERS . 'BookController.php';
         BookUpdate($match[1]);
     }
-
+// on verifie si l'url = /books et que la requete est en methode post
     elseif (preg_match('#^\/books$#',$url) && $method == "POST") {
         require CONTROLLERS . "BookController.php";
         bookStore();
     } 
-
+// on verifie si l'url = /books/create
     elseif (preg_match('#^\/books\/create$#',$url)) {
         require CONTROLLERS . 'BookController.php';
         bookCreate();
     }
-
+// on verifie si l'url = /books
     elseif ($url == '/books') {
         require CONTROLLERS . 'BookController.php';
         booksGet();
     } 
-    
+// on verifie si l'url = /books/nomDuBook
     elseif (preg_match('#^\/books\/([a-zA-Z0-9_-]+)$#',$url,$match)) {
         require CONTROLLERS . 'BookController.php';
         bookShow($match[1]);
     } 
-
+// on verifie si l'url = /books/nomDuBook/edit
     elseif (preg_match('#^\/books\/([a-zA-Z0-9_-]+)\/edit$#',$url,$match)) {
         require CONTROLLERS . 'BookController.php';
         bookEdit($match[1]);
