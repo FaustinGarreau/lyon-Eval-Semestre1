dcb6a643398b540f07a2ef5148f801c471588226
SimonCROS <scros@edenschool.fr>
2019-12-17 09:16:57 +0100

    SQL

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE eval_s1;
+USE eval_s1;
+CREATE TABLE book (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    `date` DATE NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT uq_book UNIQUE(title, author)
+);
